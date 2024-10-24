1c4d914f089ea27564cbd4a6355593014e37e92d
unknown <rameller20953@iesjoanramis.orggit config --global user.name Rafeldcexitgit config -- global user.email rameller20953@iesjoanramis.org>
2024-10-24 18:15:26 +0200

    Comentarios javadoc añadidos

--- funcionsuma.java
+++ funcionsuma.java
@@ -57,14 +57,7 @@ public class FuncionSuma {
 		*/
 		private static int suma (int sumando1, int sumando2) {
 			return sumando1+sumando2;
-		}
-		/**
-		*Funcion principal del programa
-		* /
-		public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		
-		
+		}		
 		in.close();
 
 }
